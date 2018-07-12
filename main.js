a5ca11da372866ba26b13df4f067ff585d344c05
Brad Traversy <traversymedia@gmail.com>
2018-07-12 14:25:49 -0400

    Fetch and display channel data

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Options
-const CLIENT_ID = 'YOUT_CLIENT_ID';
+const CLIENT_ID = 'YOUR_CLIENT_ID';
 const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
@@ -13,6 +13,15 @@ const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
 const defaultChannel = 'techguyweb';
+
+// Form submit and change channel
+channelForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const channel = channelInput.value;
+
+  getChannel(channel);
+});
 
 // Load auth2 library
 function handleClientLoad() {
@@ -63,7 +72,49 @@ function handleSignoutClick() {
   gapi.auth2.getAuthInstance().signOut();
 }
 
+// Display channel data
+function showChannelData(data) {
+  const channelData = document.getElementById('channel-data');
+  channelData.innerHTML = data;
+}
+
 // Get channel from API
 function getChannel(channel) {
-  console.log(channel);
+  gapi.client.youtube.channels
+    .list({
+      part: 'snippet,contentDetails,statistics',
+      forUsername: channel
+    })
+    .then(response => {
+      console.log(response);
+      const channel = response.result.items[0];
+
+      const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</li>
+          <li class="collection-item">Views: ${numberWithCommas(
+            channel.statistics.viewCount
+          )}</li>
+          <li class="collection-item">Videos: ${numberWithCommas(
+            channel.statistics.videoCount
+          )}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+      `;
+      showChannelData(output);
+    })
+    .catch(err => alert('No Channel By That Name'));
+}
+
+// Add commas to number
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
